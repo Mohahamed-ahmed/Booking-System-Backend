56e64903816150bf11af717921db45c9cf6a64e4
mohamed-ahmed <mohamed20737@fci.bu.edu.eg>
2026-05-11 15:48:50 +0300

    small fix for deployment

--- app.js
+++ app.js
@@ -13,9 +13,10 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
