93507d5d02b41064df1a720784273aa76191ff24
mohamed-ahmed <mohamed20737@fci.bu.edu.eg>
2026-05-13 16:47:12 +0300

    small fix for deployment

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controller/user')
+const { isAuth } = require('../middleware/isAuth')
+const {isAdmin} = require('../middleware/isAdmin')
 
-router.get('/profile', userController.getUserProfile)
-router.get('/user/:id', userController.getuserById)
+router.get('/profile', isAuth, userController.getUserProfile)
+router.get('/user/:id', isAuth, isAdmin, userController.getuserById)
 
 module.exports = router;
